ff286e624fd46f44b69f7e140103afa211bad894
Vanessa K Lee <vanessaklee@vanessas-mbp-2.attlocal.net>
2022-07-24 15:06:20 -0500

    steps towards normalization and proper target

--- lib/tensors_example.ex
+++ lib/tensors_example.ex
@@ -3,14 +3,16 @@ defmodule TensorsExample do
   """
   require Axon
 
-  defp build_model(input_shape1, input_shape2, input_shape3) do
+  defp build_model(input_shape1, input_shape2, input_shape3, input_shape4) do
     inp1 = Axon.input(input_shape1, "open")
-    inp2 = Axon.input(input_shape2, "high")
-    inp3 = Axon.input(input_shape3, "low")
+    inp2 = Axon.input(input_shape2, "close")
+    inp3 = Axon.input(input_shape3, "high")
+    inp4 = Axon.input(input_shape4, "low")
 
     inp1
     |> Axon.concatenate(inp2)
     |> Axon.concatenate(inp3)
+    |> Axon.concatenate(inp4)
     |> Axon.dense(8, activation: :tanh)
     |> Axon.dense(1, activation: :sigmoid)
   end
@@ -19,6 +21,10 @@ defmodule TensorsExample do
   defp batch do
     open = stream_and_decode()
       |> Enum.map(fn {_, %{"Open" => open}} -> [String.to_float(open)] end)
+      |> Enum.to_list()
+      |> Nx.tensor()
+    close = stream_and_decode()
+      |> Enum.map(fn {_, %{"Close" => close}} -> [String.to_float(close)] end)
       |> Enum.to_list()
       |> Nx.tensor()
     high = stream_and_decode()
@@ -30,11 +36,27 @@ defmodule TensorsExample do
       |> Enum.to_list()
       |> Nx.tensor()
 
-    targets = stream_and_decode()
-      |> Enum.map(fn {_, %{"Close" => close}} -> [String.to_float(close)] end)
+    # time series normalization equation: (x - min) / (max - min)
+    # https://tcoil.info/normalize-stock-prices-and-time-series-data-with-python-2/
+    # x - price from input time series
+    # min - minimum price in the time seies
+    # max - maximum price of the time series
+    target = stream_and_decode()
+      |> Enum.map(fn {_, %{"Close" => close, "Low" => low, "High" => high}} ->
+        x = String.to_float(close)
+        min = String.to_float(low)
+        max = String.to_float(high)
+        [(x - min) / (max - min)]
+      end)
       |> Nx.tensor()
 
-    {%{"open" => open, "high" => high, "low" => low}, targets}
+    # target = stream_and_decode()
+    #   |> Enum.map(fn {_, %{"Close" => close}} -> [String.to_float(close)] end)
+    #   |> Nx.tensor()
+
+    # {%{"open" => open, "high" => high, "low" => low}, target}
+
+    {%{"open" => open, "high" => high, "low" => low, "close" => close}, target}
   end
 
   defp stream_and_decode() do
@@ -51,7 +73,7 @@ defmodule TensorsExample do
 
   def start do
     # nil means the value will be provided later (done on the next line)
-    model = build_model({nil, 1}, {nil, 1}, {nil, 1})
+    model = build_model({nil, 1}, {nil, 1}, {nil, 1}, {nil, 1})
     data = Stream.repeatedly(&batch/0)
 
     # As the data comes in here, it matches the model.
@@ -60,8 +82,9 @@ defmodule TensorsExample do
     IO.inspect(
       Axon.predict(model, model_state, %{
         "open" => Nx.tensor([[0]]),
-        "high" => Nx.tensor([[1]]),
-        "low" => Nx.tensor([[2]])
+        "close" => Nx.tensor([[1]]),
+        "high" => Nx.tensor([[2]]),
+        "low" => Nx.tensor([[3]])
       })
     )
   end
